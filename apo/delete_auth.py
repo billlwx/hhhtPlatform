a550b1b4319b3667359510da98901e6c795a58c3
yarphy <675584450@qq.com>
2017-09-23 17:18:49 +0800

    清除认证信息的redis,增加返回

--- apo/delete_auth.py
+++ apo/delete_auth.py
@@ -7,5 +7,6 @@ from django.core.cache import cache
 def delete_auth(request):
     id = request.GET['userId']
     key = 'auth_status::'+id
+    print key
     cache.delete(key)
     return HttpResponse({'succes'})
