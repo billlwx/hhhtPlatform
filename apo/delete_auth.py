ee9b768d78a77ae29e889930152cb71d362e8afe
yarphy <675584450@qq.com>
2017-09-23 18:22:43 +0800

    redis

--- apo/delete_auth.py
+++ apo/delete_auth.py
@@ -2,12 +2,14 @@ from MysqldbHelper import *
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.cache import cache
+from django_redis import get_redis_connection
 
 
 def delete_auth(request):
     id = request.GET['userId']
     key = 'auth_status::'+id
     print key
-    cache.delete("auth_status::136249")
+    con = get_redis_connection("default")
+    con.delete("auth_status::136249")
     return HttpResponse("success")
 
--- apo/redis.py
+++ apo/redis.py
@@ -1,6 +1,8 @@
 import redis
 import time
 ## Connect local redis service
+
+
 client =redis.Redis(host='172.18.225.181',port=6379)
 print "Connection to server successfully!"
 dicKeys = client.keys("*")
