ba31d969d695c89a3c9969e04b3fafb4133b9c89
yarphy <675584450@qq.com>
2017-09-23 19:25:01 +0800

    redis

--- apo/delete_auth.py
+++ apo/delete_auth.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from django.conf import settings
 from django.core.cache import cache
 from django_redis import get_redis_connection
+import redis
 
 
 def delete_auth(request):
+    r = redis.ConnectionPool(host='172.18.225.181',port=6379)
     id = request.GET['userId']
     key = 'auth_status::'+id
     print key
-    cache.delete_pattern(key)
+    r.delete(key)
     return HttpResponse("success")
 
