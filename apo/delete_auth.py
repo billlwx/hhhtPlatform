973b191b885dc29429eb0913b1a07061307488ac
yarphy <675584450@qq.com>
2017-09-23 19:31:56 +0800

    redis

--- apo/delete_auth.py
+++ apo/delete_auth.py
@@ -7,7 +7,8 @@ import redis
 
 
 def delete_auth(request):
-    r = redis.ConnectionPool(host='172.18.225.181',port=6379)
+    pool = redis.ConnectionPool(host='172.18.225.181',port=6379)
+    r = redis.StrictRedis(connection_pool=pool)
     id = request.GET['userId']
     key = 'auth_status::'+id
     print key
