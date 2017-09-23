5ef1bf913c9a9d554f7af5f7a03016fec2faf4d4
yarphy <675584450@qq.com>
2017-09-23 18:48:45 +0800

    redis

--- apo/delete_auth.py
+++ apo/delete_auth.py
@@ -9,6 +9,6 @@ def delete_auth(request):
     id = request.GET['userId']
     key = 'auth_status::'+id
     print key
-    cache.delete_pattern("auth_status::136249")
+    cache.delete_pattern(key)
     return HttpResponse("success")
 
