c027a8084e4f7910ad60c0fa9fe99d327631fb6e
zhouyahui <675584450@qq.com>
2017-09-27 11:25:36 +0800

    添加静态资源

--- hhht/settings.py
+++ hhht/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
-# 设置图片等静态文件的路径
+
 STATICFILES_DIRS = (
     ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
     ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
