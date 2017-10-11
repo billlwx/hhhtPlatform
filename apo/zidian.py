679f2a8b619a4f213647e726005b7236e4afd3c4
zhouyahui <675584450@qq.com>
2017-10-11 17:04:35 +0800

    样式

--- apo/zidian.py
+++ apo/zidian.py
@@ -1,3 +1,8 @@
 cgg_test_db = {
-    '119.23.218.196', 33066, 'admin', 'admin#ROOT@ha', 'miloan'}
+    'DB_HOST': '119.23.218.196',
+    'DB_PORT':33066,
+    'DB_USER':'admin',
+    'DB_PWD': 'admin#ROOT@ha',
+    'DB_NAME':'miloan'
+}
 
