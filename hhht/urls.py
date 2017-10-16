ab7b8ce748fb0822fbdd0f4f5e6ed6c25eddd12d
zhouyahui <675584450@qq.com>
2017-10-16 15:56:58 +0800

    url

--- hhht/urls.py
+++ hhht/urls.py
@@ -28,7 +28,6 @@ import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/$',views.index),
-    url(r'^index/single.html$', views.single),
     url(r'^tools/$', views.tools),
     url(r'^selectUid/$', selectUid.selectUid),
     url(r'^deleteUserInfo/$', deleteUserInfo.deleteUserInfo),
