405df5d9418221be116b759e90347b7373e75104
liwenxin <liwenxin@188w.com>
2017-10-19 11:17:21 +0800

    避开风控进件

--- myrisktest/my_producer.py
+++ myrisktest/my_producer.py
@@ -37,3 +37,28 @@ class MyProducer:
         except (BaseException), e:
             print e
 
+    def producerlendrulesmq(self,MQ):
+        exchange = 'ex-case-audit'
+        # queue='anti_fraud_request_queue'
+        # exchange='py_exchange'
+        # queue='py_queue'
+        try:
+           connection = pika.BlockingConnection(pika.ConnectionParameters(
+            '172.18.225.179'))
+           channel = connection.channel()
+           channel.exchange_declare(exchange=exchange, exchange_type='fanout', durable=True)
+        except (BaseException), e:
+            print e
+        # 声明queue
+        # channel.queue_declare(queue=queue)
+        # channel.queue_bind(exchange=exchange,queue=queue)
+        # n RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
+        try:
+            channel.basic_publish(exchange=exchange,
+                                  routing_key='',
+                                  body=MQ)
+            connection.close()
+        except (BaseException), e:
+            print e
+
+
--- myrisktest/producer_client.py
+++ myrisktest/producer_client.py
@@ -5,6 +5,8 @@
 import sys
 from my_producer import *
 from django.http import HttpResponse
+from apo.zidian import *
+from apo.MysqldbHelper import *
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -13,3 +15,21 @@ def sendriskmq(request):
     producer = MyProducer()
     producer.producerriskmq()
     return HttpResponse('success')
+
+def sendlendrulesmq(request):
+    handleResult = request.GET.get('handleResult')
+    mobile = request.GET.get('mobile')
+    if mobile == "":
+       return HttpResponse("手机号不能为空")
+    else:
+        sql = "SELECT case_no FROM `case` WHERE mobile=%s ORDER BY id desc limit 1;" % (mobile)
+        db = DB(**cgg_test_db)
+        fc = db.query(sql)
+    if fc == ():
+        return HttpResponse('请输入正确的手机号')
+    for row in fc:
+        taskID = row[0]
+    MQ = '''{"engineDetails":"测试组模拟数据","handleResult":"%s","object":[{"timestamp":"100","type":"3"},{"timestamp":"100","type":"2"},{"timestamp":"100","type":"1"},{"timestamp":"100","type":"4"},{"timestamp":"100","type":"5"},{"timestamp":"100","type":"6"},{"timestamp":"100","type":"7"},{"timestamp":"100","type":"8"},{"timestamp":"100","type":"9"},{"timestamp":"100","type":"10"},{"timestamp":"100","type":"11"}],"phoneNum":"%s","score_info":[{"creditAmount":-1,"score":500,"scorecard_type":"1","startTime":1508226996660,"validDays":90}],"taskID":"%s"}''' % (handleResult, mobile, taskID)
+    producer = MyProducer()
+    producer.producerlendrulesmq(MQ)
+    return HttpResponse('推送成功')
