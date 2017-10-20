c442be61459fface608b5a0f89770fc428fc0df6
zhouyahui <675584450@qq.com>
2017-10-20 11:57:38 +0800

    催收逾期数据构造

--- apo/duedate.py
+++ apo/duedate.py
@@ -22,6 +22,7 @@ def duedate(request):
 
     selectcollection_no = 'select collection_no from overdue_collection ORDER BY collection_no desc  limit 1'
     oldcolno = db.query(selectcollection_no)
+    print oldcolno
     collection_no = str(int(oldcolno)+1)
     overdue_collection = "INSERT INTO `overdue_collection` (`collection_no`, `contract_no`, `case_no`, `collection_status`, `collector`, `assign_status`, `assign_time`, `loan_count`, `overdue_days`, `late_interest`, `overdue_interest`, `communicate_comment`, `communicate_result`, `created_by`, `last_modified_by`, `created_date`, `last_modified_date`, `collector_id`) VALUES ('%s', '%s', '10480', '0', NULL, '0', '1980-01-01 00:00:00', '21', '1', '0.00', '10.00', NULL, NULL, NULL, NULL, '%s', '%s', NULL)" % (collection_no,contract_no,datenow,datenow)
 
