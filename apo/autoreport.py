5d2866f577e32c29a0fd50c5ecac54ba04449e3e
yarphy <675584450@qq.com>
2017-10-31 16:33:27 +0800

    test

--- apo/autoreport.py
+++ apo/autoreport.py
@@ -7,12 +7,10 @@ import os,sys
 def autoreportlist(request):
     path = '/apps/data/act/autoreport'
     #  path = '/Users/zhouyahui/code/hhht/static/report'
-    # f_list = os.listdir(path)
     reporturl = 'http://119.23.218.196:7081/'
     reportlist = []
-    for filenames in os.listdir(path):
-        filename = filenames[0]
+    for file in os.listdir(path):
         reportlist.append("filename=%s,url=%s%s;" % \
-                        (filename,reporturl,filename))
-        print filename
+                        (file,reporturl,file))
+        print file
         return HttpResponse(reportlist)
