1636ae86fd4ec0e1441f39d60b8153ca47b70ff9
yarphy <675584450@qq.com>
2017-10-31 16:14:34 +0800

    test

--- apo/autoreport.py
+++ apo/autoreport.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 import os,sys
 
 def autoreportlist(request):
-    path = '/apps/data/act/autoreport/'
-    # # path = '/Users/zhouyahui/code/hhht/static/report'
+    path = "/apps/data/act/autoreport/"
+    #  path = '/Users/zhouyahui/code/hhht/static/report'
     # f_list = os.listdir(path)
     reporturl = 'http://119.23.218.196:7081/'
     reportlist = []
-    for filenames in os.walk(path):
+    for filenames in os.listdir(path):
         for filename in filenames:
             reportlist.append("filename=%s,url=%s+%s;" % \
                         (filename,reporturl,filename))
